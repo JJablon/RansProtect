c0a367a7b2290db6006934dbc29d6db2f8d3dee4
JJablons <jackeneczek@gmail.com>
2017-07-06 23:36:42 +0200

    init

--- RansProtect/Form1.Designer.cs
+++ RansProtect/Form1.Designer.cs
@@ -29,11 +29,142 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addToWhitelistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.IncludeSubdirectories = true;
+            this.fileSystemWatcher1.Path = "C:\\";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.Log = "RansProtect";
+            this.eventLog1.Source = "RansProtect";
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(284, 302);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(567, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(201, 302);
+            this.listBox2.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(284, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(283, 302);
+            this.treeView1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(284, 280);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(283, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(46, 17);
+            this.toolStripStatusLabel1.Text = "Refresh";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToWhitelistToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(133, 26);
+            // 
+            // addToWhitelistToolStripMenuItem
+            // 
+            this.addToWhitelistToolStripMenuItem.Name = "addToWhitelistToolStripMenuItem";
+            this.addToWhitelistToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.addToWhitelistToolStripMenuItem.Text = "Add to whitelist";
+            this.addToWhitelistToolStripMenuItem.Click += new System.EventHandler(this.addToWhitelistToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 302);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addToWhitelistToolStripMenuItem;
     }
 }
 
--- RansProtect/Form1.cs
+++ RansProtect/Form1.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Collections;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +12,361 @@ namespace RansProtect
 {
     public partial class Form1 : Form
     {
+        public bool ran = false;
+        public static long iteration = 0;
+
         public Form1()
         {
             InitializeComponent();
+            /*try
+            {
+                System.Diagnostics.EventLog.CreateEventSource("RansProtect", "RansProtect");
+            }
+            catch (Exception) { }
+            if (System.Diagnostics.EventLog.SourceExists("RansProtect"))
+            {
+                System.Diagnostics.EventLog.WriteEntry("RansProtect", "event1", System.Diagnostics.EventLogEntryType.Information);
+                //eventLog1.WriteEntry("event1", System.Diagnostics.EventLogEntryType.SuccessAudit, 6666);
+                eventLog1.WriteEvent(new System.Diagnostics.EventInstance(6666, 6666), "event1");
+            }
+            else { MessageBox.Show("aaa"); }*/
         }
+        TreeNode structure = new TreeNode("ROOT", null);
+        System.Windows.Forms.TreeNode root;
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            iteration++;
+            if (!ran) {
+                Stack<String> nodes = new Stack<String>();
+                //this.listBox1.Items.Add("Changed:" + e.FullPath);
+                FileInfo fi = new FileInfo(e.FullPath);
+                DirectoryInfo parent = fi.Directory;
+                do
+                {
+
+
+                    nodes.Push(parent.Name);
+
+
+                    parent = Directory.GetParent(parent.FullName);
+                }
+
+                while (Path.GetPathRoot(e.FullPath) != parent.FullName);
+                nodes.Push(parent.Name);
+                var reversed = nodes.ToArray();
+                //Array.Reverse(reversed);
+                var currentNode = structure;
+                foreach (string name in nodes)
+                {
+                    //listBox2.Items.Add(name);
+                    if (currentNode != null)
+                    {
+                        if (!currentNode.Children.Contains(new TreeNode(name, null))) currentNode.Children.Add(new TreeNode(name, null));
+                        else
+                        {
+                            currentNode.GetChildNode(name).Changed++;
+                        }
+                        currentNode = currentNode.GetChildNode(name);
+
+                    }
+                    else
+                    {
+                        listBox2.Items.Add("fatal error");
+                    }
+                }
+                if(this.treeView1.Nodes == null || this.treeView1.Nodes.Count == 0)
+                 root = this.treeView1.Nodes.Add("ROOT");
+
+                //ran = true;
+            }
+        }
+        private void Refresh()
+        {
+
+            TreeWalker.RefreshExpanded(structure);
+            string selected = "";
+            if(treeView1.SelectedNode != null)
+            selected = treeView1.SelectedNode.FullPath;
+            List<string> ExpandedNodes = new List<string>();
+            if (treeView1 != null && treeView1.Nodes != null && treeView1.Nodes.Count > 0 && treeView1.Nodes[0].Nodes != null && treeView1.Nodes[0].Nodes.Count > 0)
+            {
+
+
+                System.Windows.Forms.TreeNode Nodes1 = treeView1.Nodes[0];
+                structure.Expanded = true;
+                TreeWalker.collectExpandedNodes(Nodes1.Nodes[0], structure);
+            }
+
+            treeView1.Nodes.Clear();
+            if (this.treeView1.Nodes == null || this.treeView1.Nodes.Count == 0||!this.treeView1.Nodes.Contains(new System.Windows.Forms.TreeNode("ROOT")))
+                root = this.treeView1.Nodes.Add("ROOT");
+            treeView1.BeginUpdate();
+            TreeWalker.RefreshAdded(structure);
+            TreeWalker.Walk(structure, root);
+
+            treeView1.EndUpdate();
+            treeView1.Update();
+            treeView1.Refresh();
+
+            System.Windows.Forms.TreeNode Nodes2 = treeView1.Nodes[0];
+            treeView1.Nodes[0].Expand();
+            TreeWalker.ExpandNodes(Nodes2.Nodes[0], structure);
+            if(selected != "")
+             treeView1.SelectedNode =  TreeWalker.GetNodeFromPath(treeView1.Nodes, selected, treeView1) ;
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+            timer1.Interval = 1000;
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+            TreeWalker.RefreshStats(structure);
+            Refresh();
+
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addToWhitelistToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.TreeNode tn = this.treeView1.SelectedNode;
+
+        }
+    }
+
+    static class TreeWalker
+    {
+        public static System.Windows.Forms.TreeNode GetNodeFromPath(TreeNodeCollection nodes, string path, TreeView tvPD2)
+        {
+            System.Windows.Forms.TreeNode foundNode = null;
+            foreach (System.Windows.Forms.TreeNode tn in nodes)
+            {
+                if (tn.FullPath == path)
+                {
+                    tvPD2.SelectedNode = tn;
+                    tvPD2.SelectedNode.EnsureVisible();
+                    tvPD2.Focus();
+                    return tn;
+                }
+                else if (tn.Nodes.Count > 0)
+                {
+                    foundNode = GetNodeFromPath(tn.Nodes, path,tvPD2);
+                }
+                if (foundNode != null)
+                    return foundNode;
+            }
+            return null;
+        }
+
+
+        public static void collectExpandedNodes(System.Windows.Forms.TreeNode Nodes, TreeNode tn)
+        {
+            if(Nodes != null && Nodes.Nodes != null && Nodes.Nodes.Count > 0)
+            { 
+                
+                //tn.Expanded = true;
+                for (int i = 0; i < Nodes.Nodes.Count; i++)
+                {
+                    System.Windows.Forms.TreeNode checknode = Nodes.Nodes[i];
+                    if (tn != null && tn.GetChildNode(checknode.Text) != null
+                      )
+                    {
+                        if (checknode.IsExpanded)
+                        {
+                            tn.GetChildNode(checknode.Text).Expanded = true;
+                            collectExpandedNodes(checknode, tn.GetChildNode(checknode.Text));
+                        }
+                    }
+                }
+                
+            }
+            
+        }
+
+
+        public static void ExpandNodes(System.Windows.Forms.TreeNode Nodes, TreeNode tn)
+        {
+            if (Nodes != null && Nodes.Nodes != null && Nodes.Nodes.Count > 0)
+            {
+                //Nodes = Nodes.Nodes[0];
+                for (int i = 0; i < Nodes.Nodes.Count; i++)
+                {
+                    System.Windows.Forms.TreeNode checknode = Nodes.Nodes[i];
+                    
+                        if (tn!=null&& tn.Expanded == true) Nodes.Expand(); //checknode
+                        //tn.GetChildNode(checknode.Text).Expanded = true;
+                    if (tn != null &&  tn.GetChildNode(checknode.Text) != null   )
+                    {
+                        ExpandNodes(checknode, tn.GetChildNode(checknode.Text));
+                    }
+                }
+
+            }
+
+        }
+
+
+        
+        public static void Walk ( TreeNode tn, System.Windows.Forms.TreeNode tnc )
+        {
+            int dummy = 0;
+            if (tnc != null)
+            {
+                System.Windows.Forms.TreeNode temp = null;
+                //if (tnc.Nodes != null && !tnc.Nodes.Contains(new System.Windows.Forms.TreeNode(tn.Name))) temp = tnc.Nodes.Add(tn.Name);
+
+                if (tn.AddedToTree == false) { tn.AddedToTree = true; temp = tnc.Nodes.Add(tn.Name);// tnc.TreeView.BeginUpdate();
+                }
+                else
+                {
+                    foreach (System.Windows.Forms.TreeNode iterating in tnc.Nodes)
+                    {
+                        if (iterating.Name == tn.Name) temp = iterating;
+                    }
+                    
+                }
+
+                if (tn.Changed > 10) temp.ForeColor = System.Drawing.Color.Green;
+                if (tn.Changed > 50) temp.ForeColor = System.Drawing.Color.Yellow;
+                if (tn.Changed > 100) temp.ForeColor = System.Drawing.Color.Orange;
+                if (tn.Changed > 200) temp.ForeColor = System.Drawing.Color.DarkOrange;
+                if (tn.Changed > 1000) temp.ForeColor = System.Drawing.Color.OrangeRed;
+                if (tn.Changed > 2000) temp.ForeColor = System.Drawing.Color.Red;
+                if (tn.Children != null && tn.Children.Count != 0)
+                    foreach (TreeNode node in tn.Children)
+                    {
+                        TreeWalker.Walk(node, temp);
+                        if (Form1.iteration > 100)
+                            {
+                             dummy++;
+                            }
+                    }
+                 
+
+            }
+
+        }
+
+
+
+
+        public static void RefreshAdded(TreeNode tn)
+        {
+                if (tn.Children != null && tn.Children.Count != 0)
+                    foreach (TreeNode node in tn.Children)
+                    {
+                        node.AddedToTree = false;
+                        TreeWalker.RefreshAdded(node);
+                    }
+                tn.AddedToTree = false;
+            }
+        public static void RefreshStats(TreeNode tn)
+        {
+            if (tn.Children != null && tn.Children.Count != 0)
+                foreach (TreeNode node in tn.Children)
+                {
+                    node.Changed = 0;
+                    TreeWalker.RefreshStats(node);
+                }
+            tn.AddedToTree = false;
+        }
+
+        public static void RefreshExpanded(TreeNode tn)
+        {
+            if (tn.Children != null && tn.Children.Count != 0)
+                foreach (TreeNode node in tn.Children)
+                {
+                    node.Expanded = false;
+                    TreeWalker.RefreshExpanded(node);
+                }
+            tn.AddedToTree = false;
+        }
+
+
+
+
+
+
+    }
+
+
+    class TreeNode : IEquatable<TreeNode>
+    {
+        public int id;
+        public bool AddedToTree { get; set; }
+        public string Name { get; set; }
+        public int Changed { get; set; }
+        public bool Expanded { get; set; }
+        //public TreeNode FirstChild { get;  set; }
+        public List<TreeNode> Children { get; set; }
+        //public TreeNode NextSibling { get;  set; }
+        public TreeNode(string data, TreeNode firstChild//, TreeNode nextSibling
+            )
+        {
+            this.AddedToTree = false;
+            this.Name = data;
+            //this.FirstChild = firstChild;
+            Children = new List<TreeNode>();
+            //this.NextSibling = nextSibling;
+            Changed = 1;
+            Expanded = false;
+        }
+
+
+
+        public TreeNode GetChildNode (string name)
+        {
+            if(this.Children.Contains(new TreeNode(name, null)))
+            {
+                int cnt = 0;
+                    foreach (var o in this.Children)
+                {
+                    if (o.Name == name) return this.Children[cnt];
+                    cnt++;
+                }
+                return null;
+
+            }
+            else
+            {
+                return null;
+            }
+            
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            TreeNode objAsPart = obj as TreeNode;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+        public override int GetHashCode()
+        {
+            return id;
+        }
+        public bool Equals(TreeNode other)
+        {
+            if (other == null) return false;
+            return (this.Name.Equals(other.Name));
+        }
+
+
+
+
     }
 }
